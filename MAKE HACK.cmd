cfd8109e2e6edac9f31d840babd5dfcb6b11e827
U-COLORZ-STRIX\Crazycolorz5 <Crazycolorz5@gmail.com>
2017-06-21 21:07:19 -0400

    Initial commit. Added script.

---
+++ MAKE HACK.cmd
@@ -0,0 +1,13 @@
+﻿echo off
+cd %~dp0
+echo on
+
+copy FE8_clean.gba "Fire Emblem Destiny.gba"
+
+c2ea
+
+cd "%~dp0Event Assembler"
+
+Core A FE8 "-output:%~dp0Fire Emblem Destiny.gba" "-input:%~dp0ROM Buildfile.txt"
+
+pause
